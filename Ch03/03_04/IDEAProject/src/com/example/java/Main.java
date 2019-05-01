74af6322ca8684eca6fa81bba6ca442928b784f0
Anwar <anwar.saleeby@gmail.com>
2019-05-01 08:55:45 -0400

    After about 2 weeks of programming, here is my first commit. -Anwar

---
+++ Ch03/03_04/IDEAProject/src/com/example/java/Main.java
@@ -0,0 +1,11 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello from Idea!");
+	    String aString = "Anwar";
+        System.out.println("Your name is :" + aString);
+
+    }
+}
